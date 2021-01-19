04a02ae5d7556dc08d9c5c7d0edddf0e78213551
kylejschulz <kylschulz@gmail.com>
2021-01-18 21:57:27 -0500

    Add open_handle method

--- lib/fileio.rb
+++ lib/fileio.rb
@@ -1,6 +1,6 @@
 require_relative 'enigma'
 class FileIO
-  attr_reader :read, :write, :key, :date
+  attr_reader :read, :write, :key, :date, :enigma
   def initialize(read, write, key = '', date = '')
     @read = read
     @write = write
@@ -13,26 +13,26 @@ class FileIO
     File.open(@read, 'r')
   end
 
-  def read_handle
-    handle.read.downcase
-  end
-
-  def close_handle
-    handle.close
-  end
-
-  #encrypt or decrypt
-
-  def open_writer
-    File.open(write, 'w')
-  end
-
-  def  write_writer
-    writer.write()
-  end
-
-  def close_writer
-    writer.close
-  end
+  # def read_handle
+  #   handle.read.downcase
+  # end
+  #
+  # def close_handle
+  #   handle.close
+  # end
+  #
+  # #encrypt or decrypt
+  # 
+  # def open_writer
+  #   File.open(write, 'w')
+  # end
+  #
+  # def  write_writer
+  #   writer.write()
+  # end
+  #
+  # def close_writer
+  #   writer.close
+  # end
 # encrypted_text = @enigma.encrypt(text_to_encrypt)
 end
