9752e527099429794994258a82a00c4ca1cf8181
kylejschulz <kylschulz@gmail.com>
2021-01-18 21:42:48 -0500

    Setup fileio test

--- test/fileio_test.rb
+++ test/fileio_test.rb
@@ -2,6 +2,9 @@ require_relative 'test_helper'
 
 class FileIOTest < Minitest::Test
   def setup
+    read = './little_brother'
+    write = './encrypted.txt'
+    @filio = FileIO.new()
   end
   def test_it_exists
   assert_instance_of
