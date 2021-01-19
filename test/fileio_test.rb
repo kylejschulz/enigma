9547ae976c59bf69b72d0d7b16cffa9ec83898cf
kylejschulz <kylschulz@gmail.com>
2021-01-18 21:46:09 -0500

    test fileio has attributes

--- test/fileio_test.rb
+++ test/fileio_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 class FileIOTest < Minitest::Test
   def setup
-    read = './little_brother'
+    read = './little_brother.txt'
     write = './encrypted.txt'
     @filio = FileIO.new(read, write)
   end
@@ -10,9 +10,12 @@ class FileIOTest < Minitest::Test
   def test_it_exists
     assert_instance_of FileIO, @fileio
   end
-  
+
   def test_it_has_attributes
+    assert_equal './little_brother.txt', @fileio.read
+    assert_equal './encrypted.txt', @fileio.write
   end
+  
   def test_it_can_have_different_attributes
   end
 end
