808f156e35bd7a9aa34ddd72944fcce925281f2f
kylejschulz <kylschulz@gmail.com>
2021-01-18 22:40:31 -0500

    finish encrypt runner file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,15 +3,10 @@ require_relative './enigma'
 @fileio = FileIO.new(ARGV[0], ARGV[1])
 
 @fileio.open_read_close
-@fileio.enigma.encrypt(@fileio.read_handle)
-@fileio.open_writer 
+encryption = @fileio.enigma.encrypt(@fileio.read_handle)
+@fileio.open_writer
+@fileio.write_writer(encryption)
+@fileio.close_writer
 
 
-text_to_encrypt = handle.read.downcase.strip.chomp
-handle.close
-
-encrypted_text = @enigma.encrypt(text_to_encrypt)
-writer = File.open(ARGV[1], 'w')
-writer.write(encrypted_text)
-writer.close
-puts "Created '#{ARGV[1]}' with the key #{encrypted_text[:key]} and date #{encrypted_text[:date]}"
+puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
