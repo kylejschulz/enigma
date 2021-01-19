88ed3af0890e09b1a38f0a1e0b3d54b74dad5845
kylejschulz <kylschulz@gmail.com>
2021-01-18 21:40:39 -0500

    Add fileio test

---
+++ test/fileio_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+
+class FileIOTest < Minitest::Test
+  def setup
+  end
+  def test_it_exists
+  assert_instance_of
+  end
+  def test_it_has_attributes
+  end
+  def test_it_can_have_different_attributes
+  end
+end
