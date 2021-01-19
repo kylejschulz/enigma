994fb8420f55adad48d30e48394dfc6c27f0dc1d
kylejschulz <kylschulz@gmail.com>
2021-01-18 21:54:14 -0500

    Add test for open_handle

--- test/fileio_test.rb
+++ test/fileio_test.rb
@@ -2,9 +2,9 @@ require_relative 'test_helper'
 
 class FileIOTest < Minitest::Test
   def setup
-    read = './little_brother.txt'
-    write = './encrypted.txt'
-    @filio = FileIO.new(read, write)
+    @read = './little_brother.txt'
+    @write = './encrypted.txt'
+    @fileio = FileIO.new(@read, @write)
   end
 
   def test_it_exists
@@ -14,8 +14,13 @@ class FileIOTest < Minitest::Test
   def test_it_has_attributes
     assert_equal './little_brother.txt', @fileio.read
     assert_equal './encrypted.txt', @fileio.write
+    assert_equal '', @fileio.key
+    assert_equal '', @fileio.date
+    assert_instance_of Enigma, @fileio.enigma
   end
-  
-  def test_it_can_have_different_attributes
+
+  def test_it_can_open_handle
+    expected = 'One morning I woke up'
+    assert_equal expected, @fileio.open_handle 
   end
 end
