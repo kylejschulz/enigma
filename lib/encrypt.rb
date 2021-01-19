58270d357fad2ae01c8456352288be28b4998768
kylejschulz <kylschulz@gmail.com>
2021-01-18 22:48:31 -0500

    Troubleshoot encrypt runner file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,16 @@
 require_relative './enigma'
+require_relative 'fileio'
+# ruby lib/encrypt.rb little_brother.txt just_encrypted.txt
 
 @fileio = FileIO.new(ARGV[0], ARGV[1])
 
-@fileio.open_read_close
-encryption = @fileio.enigma.encrypt(@fileio.read_handle)
-@fileio.open_writer
-@fileio.write_writer(encryption)
-@fileio.close_writer
+a = @fileio.open_handle
+b = @fileio.read_handle
+c = @fileio.close_handle
+encryption = @fileio.enigma.encrypt(b)
+f = @fileio.open_writer
+g = @fileio.write_writer(encryption)
+h = @fileio.close_writer
 
 
 puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
